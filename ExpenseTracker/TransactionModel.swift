03d3206dcd5147d760b3413520465df23ce3dcc8
jwpettit <jwpettit@me.com>
2023-04-13 14:39:17 -0700

    Created TransactionModel with models for transaction types.

---
+++ ExpenseTracker/TransactionModel.swift
@@ -0,0 +1,29 @@
+//
+//  TransactionModel.swift
+//  ExpenseTracker
+//
+//  Created by Jeffrey Pettit on 4/13/23.
+//
+
+import Foundation
+
+struct Transaction: Identifiable {
+    let id: Int
+    let date: String
+    let institution: String
+    let account: String
+    var merchant: String
+    let amount: Double
+    let type: TransactionType.RawValue
+    var categoryId: Int
+    var category: String
+    let isPending: Bool
+    var isTransfer: Bool
+    var isExpense: Bool
+    var isEdited: Bool
+}
+
+enum TransactionType: String {
+    case debit = "debit"
+    case credit = "credit"
+}
