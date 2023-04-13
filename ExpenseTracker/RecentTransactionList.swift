5b9df3cad97ed0e7a52958348af3847819396fed
jwpettit <jwpettit@me.com>
2023-04-13 16:03:08 -0700

    Created header to Transaction List

---
+++ ExpenseTracker/RecentTransactionList.swift
@@ -0,0 +1,45 @@
+//
+//  RecentTransactionList.swift
+//  ExpenseTracker
+//
+//  Created by Jeffrey Pettit on 4/13/23.
+//
+
+import SwiftUI
+
+struct RecentTransactionList: View {
+    var body: some View {
+        VStack {
+            HStack {
+                // MARK: Header Title
+                Text("Recent Transactions")
+                    .bold()
+                
+                Spacer()
+                
+                // MARK: Header Link
+                NavigationLink {
+                    
+                } label: {
+                    HStack(spacing: 4) {
+                        Text("See All")
+                        Image(systemName: "chevron.right")
+                    }
+                    .foregroundColor(Color.text)
+                }
+                
+            }
+            .padding(.top)
+        }
+        .padding()
+        .background(Color.systemBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: Color.primary.opacity(0.2), radius: 10, x: 0, y: 5)
+    }
+}
+
+struct RecentTransactionList_Previews: PreviewProvider {
+    static var previews: some View {
+        RecentTransactionList()
+    }
+}
