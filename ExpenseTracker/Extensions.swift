d011b9374789c6f0d804c9d47c7fa1ab0565708c
jwpettit <jwpettit@me.com>
2023-04-13 14:23:11 -0700

    Initial Commit. Created system colors and color variables

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Jeffrey Pettit on 4/13/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
