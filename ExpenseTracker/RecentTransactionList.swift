03026008f80475c4f9ee3df461a7d49351561e99
jwpettit <jwpettit@me.com>
2023-04-13 16:14:06 -0700

    Built list of recent transactions. Built logic to hide divider for last element in list of 5.

--- ExpenseTracker/RecentTransactionList.swift
+++ ExpenseTracker/RecentTransactionList.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct RecentTransactionList: View {
+    
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    
     var body: some View {
         VStack {
             HStack {
@@ -30,6 +33,16 @@ struct RecentTransactionList: View {
                 
             }
             .padding(.top)
+            
+            // MARK: Recent Transaction List
+            ForEach(Array(transactionListVM.transactions.prefix(5).enumerated()),
+                    id: \.element)
+            { index, transaction in
+                TransactionRow(transaction: transaction)
+                
+                Divider()
+                    .opacity(index == 4 ? 0 : 1)
+            }
         }
         .padding()
         .background(Color.systemBackground)
@@ -39,7 +52,15 @@ struct RecentTransactionList: View {
 }
 
 struct RecentTransactionList_Previews: PreviewProvider {
+    
+    static let transactionListVM: TransactionListViewModel = {
+        let transactionListVM = TransactionListViewModel()
+        transactionListVM.transactions = transactionListPreviewData
+        return transactionListVM
+    }()
+    
     static var previews: some View {
         RecentTransactionList()
+            .environmentObject(transactionListVM)
     }
 }
