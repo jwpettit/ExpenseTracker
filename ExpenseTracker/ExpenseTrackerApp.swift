ea62defa1e50d10f676ffe1d2c2e6a4688f94ab7
jwpettit <jwpettit@me.com>
2023-04-13 14:14:56 -0700

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Jeffrey Pettit on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
